8305e2c135d53a567340775a12d32a7083f7994e
luguanquan <757111858@qq.com>
2020-03-14 19:32:32 +0800

    @Configuration 说明

--- src/main/java/me/zoro/kitetsu/net/template/RestTemplateConfig.java
+++ src/main/java/me/zoro/kitetsu/net/template/RestTemplateConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.client.RestTemplate;
  * @author luguanquan
  * @date 2020-03-13 20:00
  *
- * 这个 @Configuration 放佛是类似以前一些 xml 的配置,设置之后里面方法的 @Bean 才能有效
+ * 这个 @Configuration 可以替换 xml 的配置,设置之后里面方法的 @Bean 才能有效
  */
 @Configuration
 public class RestTemplateConfig {
